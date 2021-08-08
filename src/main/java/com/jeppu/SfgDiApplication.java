8b381cd98722ea020cc5ee7c927635bc14459e44
sujayrj <sujayrj@gmail.com>
2021-08-08 21:52:30 +0530

    add spring context

--- src/main/java/com/jeppu/SfgDiApplication.java
+++ src/main/java/com/jeppu/SfgDiApplication.java
@@ -1,13 +1,17 @@
 package com.jeppu;
 
+import com.jeppu.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SfgDiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SfgDiApplication.class, args);
+        ApplicationContext context = SpringApplication.run(SfgDiApplication.class, args);
+        MyController myController = context.getBean("myController", MyController.class);
+        System.out.println(myController.sayHello());
     }
 
 }
