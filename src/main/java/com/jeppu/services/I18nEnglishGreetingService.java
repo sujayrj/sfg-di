8f2588954eb927330cadd95aee8bf53c7fccfdae
sujayrj <sujayrj@gmail.com>
2021-08-10 00:31:20 +0530

    default spring profile

--- src/main/java/com/jeppu/services/I18nEnglishGreetingService.java
+++ src/main/java/com/jeppu/services/I18nEnglishGreetingService.java
@@ -3,7 +3,7 @@ package com.jeppu.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("EN")
+@Profile({"EN", "default"})     //if the "spring.profiles.active=ES" is not mentioned in application.properties, then this Service Class is treated as default
 @Service("i18nService")
 public class I18nEnglishGreetingService implements GreetingService{
     @Override
